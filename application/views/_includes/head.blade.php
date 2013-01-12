f32bda0f88d8209bd0e074b5146ad0d02b9f79b5
Phil Maurer <philmau@gmail.com>
2013-01-12 02:16:53 +0000

    Add initial scale meta to html templates

--- application/views/_includes/head.blade.php
+++ application/views/_includes/head.blade.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="initial-scale=1.0">
 	<title>{{ Config::get('application.name') }} / @yield('pagetitle')</title>
 	@yield('head')
 	{{ Asset::styles() }}
